14cfdd09921ee6cad0b973727ce2e18b262c1450
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-08-08 14:59:43 -0300

    Find user by id

--- src/main/kotlin/br/com/lukinhasssss/controllers/UserController.kt
+++ src/main/kotlin/br/com/lukinhasssss/controllers/UserController.kt
@@ -6,10 +6,7 @@ import br.com.lukinhasssss.dto.response.UserResponse
 import br.com.lukinhasssss.repositories.UserRepository
 import io.micronaut.context.annotation.Value
 import io.micronaut.http.HttpResponse
-import io.micronaut.http.annotation.Body
-import io.micronaut.http.annotation.Controller
-import io.micronaut.http.annotation.Get
-import io.micronaut.http.annotation.Post
+import io.micronaut.http.annotation.*
 import io.micronaut.http.uri.UriBuilder
 import io.micronaut.validation.Validated
 import javax.validation.Valid
@@ -25,9 +22,17 @@ class UserController(
     lateinit var appUrl: String
 
     @Get
-    fun findAll(): List<UserResponse> {
-        return userRepository.findAll().map {
-            user -> UserResponse(user)
+    fun findAll(): HttpResponse<List<UserResponse>> {
+        val users = userRepository.findAll().map { user -> UserResponse(user) }
+        return HttpResponse.ok(users)
+    }
+
+    @Get("/{id}")
+    fun findById(@PathVariable id: String): HttpResponse<Any> {
+        userRepository.findById(id).let {
+            if (it.isEmpty)
+                return HttpResponse.notFound(mapOf(Pair("message", "user not found")))
+            return HttpResponse.ok(UserResponse(it.get()))
         }
     }
 
